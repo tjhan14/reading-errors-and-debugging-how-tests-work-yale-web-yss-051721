d634114c6635aceb8c2854176e76520b1ae878fc
dependable-coder-f3d7 <tj.han@yale.edu>
2021-05-31 13:07:26 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,3 +5,9 @@ first_number = 5.to_i
 second_number = 4.to_i
 
 sum = (first_number + second_number)
+
+difference = second_number - first_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
