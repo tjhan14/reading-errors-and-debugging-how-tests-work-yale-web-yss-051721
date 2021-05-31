178ea38fe4a8316f8c673dbedddaca06cdce5dcf
dependable-coder-f3d7 <tj.han@yale.edu>
2021-05-31 13:03:48 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
 
-"first_number" = 5.to.i
+first_number = 5.to.i
 
-"second_number" = 4.to.i
+second_number = 4.to.i
 
-sum = "first first_number" + "second_number"
+sum = first first_number + second_number
